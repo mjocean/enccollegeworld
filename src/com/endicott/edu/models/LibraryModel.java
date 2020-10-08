08748f9f02c534df7197215dcdb2380c0e3de705
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-10-08 19:25:50 +0530

    The library-upgrades now have a description, upgradeEvents, a max level and a bool to check if upgrade affects the interior or exterior.

--- src/com/endicott/edu/models/LibraryModel.java
+++ src/com/endicott/edu/models/LibraryModel.java
@@ -6,16 +6,13 @@ public class LibraryModel extends BuildingModel {
 
     public LibraryModel(String name){
         super(name, BuildingType.library().getType());
-        this.getUpgrades().add(new Upgrade("libraryChairs", 0, 0));
-        this.getUpgrades().add(new Upgrade("libraryDesks", 0, 0));
-        this.getUpgrades().add(new Upgrade("libraryCoffee", 0, 0));
-        this.getUpgrades().add(new Upgrade("libraryLighting", 0, 0));
-        this.getUpgrades().add(new Upgrade("librarySoundproofing", 0, 0));
-        this.getUpgrades().add(new Upgrade("libraryLightning", 0, 0));
-        this.getUpgrades().add(new Upgrade("Catalogue", 10000, 50));
-        this.getUpgrades().add(new Upgrade("Computers", 3200, 50));
-        this.getUpgrades().add(new Upgrade("Printers", 5000, 50));
-        this.getUpgrades().add(new Upgrade("Seating", 500, 50));
+        this.getUpgrades().add(new Upgrade("libraryChairs", 0, 0, "Upgraded the chairs in the library!", UpgradeEvent.getChairEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("libraryDesks", 0, 0, "Upgraded the desks in the library!", UpgradeEvent.getTableEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("libraryCoffee", 0, 0, "Upgraded the coffee machine in the library!", UpgradeEvent.getCoffeeEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("libraryLighting", 0, 0, "Upgraded the desks in the library!", UpgradeEvent.getTableEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("Catalogue", 10000, 50, "Upgraded the catalogue of books in the library!", UpgradeEvent.getCatalogueEvents(), 4, true));
+        this.getUpgrades().add(new Upgrade("Computers", 3200, 50, "Upgraded the computers in the library!", UpgradeEvent.getComputerEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("Printers", 5000, 50, "Upgraded the printers in the library!", UpgradeEvent.getPrinterEvents(), 2, true));
     }
 
     public void increaseLibraryLevel(Upgrade upgrade) {
