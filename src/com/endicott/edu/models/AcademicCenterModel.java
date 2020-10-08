ae218d5f4ad3680ef4282a7e5a196076c93ade6d
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-10-08 19:22:53 +0530

    The upgrades now have a description, upgradeEvents, a  max level and a bool to check if upgrade affects the interior or exterior.

--- src/com/endicott/edu/models/AcademicCenterModel.java
+++ src/com/endicott/edu/models/AcademicCenterModel.java
@@ -4,11 +4,12 @@ public class AcademicCenterModel extends BuildingModel {
     //inherits from BuildingModel
     public AcademicCenterModel(String name, int numStudents, String size){
         super(name, numStudents, BuildingType.academic().getType(), size);
-        this.getUpgrades().add(new Upgrade("Tables",0, 0));
-        this.getUpgrades().add(new Upgrade("Air Conditioning", 0, 0));
-        this.getUpgrades().add(new Upgrade("Vending Machines", 0, 0));
-        this.getUpgrades().add(new Upgrade("Projectors", 0, 0));
-        this.getUpgrades().add(new Upgrade("Seats", 0, 0));
+        this.getUpgrades().add(new Upgrade("libraryChairs", 0, 0, "Upgraded the chairs in classrooms!", UpgradeEvent.getChairEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("libraryDesks", 0, 0, "Upgraded the desks in classrooms!", UpgradeEvent.getTableEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("Air Conditioning", 0, 0, "Upgraded the air conditioning in classrooms!", UpgradeEvent.getAirConditioningEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("Vending Machines", 0, 0, "Upgraded the vending machines in the building!", UpgradeEvent.getVendingMachineEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("Projectors", 10000, 50, "Upgraded the projectors of classrooms!", UpgradeEvent.getProjectorEvents(), 4, true));
+
     }
 
     public void upgradeAcademicCenter(Upgrade upgrade) {
