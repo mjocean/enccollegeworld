4fedd91bf997c02901eedcd036e33c2b1eba0851
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-10-11 19:39:55 +0530

    Added Upgrades that have more variables, however commented them out due to them causing crashes!

--- src/com/endicott/edu/models/AdministrativeBldgModel.java
+++ src/com/endicott/edu/models/AdministrativeBldgModel.java
@@ -4,10 +4,10 @@ public class AdministrativeBldgModel extends BuildingModel{
     //inherits from BuildingModel
     public AdministrativeBldgModel(String name){
         super(name, BuildingType.admin().getType());
-//        this.getUpgrades().add(new Upgrade("Tables",2500, 0));
-//        this.getUpgrades().add(new Upgrade("Bursars", 205, 0));
-//        this.getUpgrades().add(new Upgrade("Cubicles", 320, 0));
-//        this.getUpgrades().add(new Upgrade("MeetingHalls", 32, 0));
+//        this.getUpgrades().add(new Upgrade("Tables", 0, 0, "Upgraded the Tables in admin offices!", UpgradeEvent.getTableEvents(), 2, true));
+        this.getUpgrades().add(new Upgrade("Bursars", 205, 0));
+//        this.getUpgrades().add(new Upgrade("Cubicles", 320, 0, "Upgraded the office cubicles in the admin", UpgradeEvent.getCubicleEvents(), 3, true));
+//        this.getUpgrades().add(new Upgrade("MeetingHalls", 320, 0, "Upgraded the meeting rooms in the admin", UpgradeEvent.getMeetingRoomEvents(), 3, true));
     }
 
     public void upgradeAdminModel(Upgrade upgrade) {
