89f46c5db0bc8e4f067902fbcf3e4dd6848c95ea
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-10-11 19:38:59 +0530

    Removed revised version of Upgrades, since they are causing crashes!

--- src/com/endicott/edu/models/AcademicCenterModel.java
+++ src/com/endicott/edu/models/AcademicCenterModel.java
@@ -1,14 +1,18 @@
 package com.endicott.edu.models;
 
+
 public class AcademicCenterModel extends BuildingModel {
     //inherits from BuildingModel
     public AcademicCenterModel(String name, int numStudents, String size){
         super(name, numStudents, BuildingType.academic().getType(), size);
-        this.getUpgrades().add(new Upgrade("libraryChairs", 0, 0, "Upgraded the chairs in classrooms!", UpgradeEvent.getChairEvents(), 2, true));
-        this.getUpgrades().add(new Upgrade("libraryDesks", 0, 0, "Upgraded the desks in classrooms!", UpgradeEvent.getTableEvents(), 2, true));
-        this.getUpgrades().add(new Upgrade("Air Conditioning", 0, 0, "Upgraded the air conditioning in classrooms!", UpgradeEvent.getAirConditioningEvents(), 2, true));
-        this.getUpgrades().add(new Upgrade("Vending Machines", 0, 0, "Upgraded the vending machines in the building!", UpgradeEvent.getVendingMachineEvents(), 2, true));
-        this.getUpgrades().add(new Upgrade("Projectors", 10000, 50, "Upgraded the projectors of classrooms!", UpgradeEvent.getProjectorEvents(), 4, true));
+
+        this.getUpgrades().add(new Upgrade("libraryChairs", 0, 0));
+        // these are causing issues!!!!!!!!!
+//        this.getUpgrades().add(new Upgrade("libraryChairs", 0, 0, "Upgraded the chairs in classrooms!", UpgradeEvent.getChairEvents(), 2, true));
+//        this.getUpgrades().add(new Upgrade("libraryDesks", 0, 0, "Upgraded the desks in classrooms!", UpgradeEvent.getTableEvents(), 2, true));
+//        this.getUpgrades().add(new Upgrade("Air Conditioning", 0, 0, "Upgraded the air conditioning in classrooms!", UpgradeEvent.getAirConditioningEvents(), 2, true));
+//        this.getUpgrades().add(new Upgrade("Vending Machines", 0, 0, "Upgraded the vending machines in the building!", UpgradeEvent.getVendingMachineEvents(), 2, true));
+//        this.getUpgrades().add(new Upgrade("Projectors", 10000, 50, "Upgraded the projectors of classrooms!", UpgradeEvent.getProjectorEvents(), 4, true));
 
     }
 
