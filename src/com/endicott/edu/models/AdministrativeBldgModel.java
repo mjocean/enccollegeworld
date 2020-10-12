a1e2693dbddfec3e6debea9967949c0981c6ef18
jamyang-tamang <61526136+jamyang-tamang@users.noreply.github.com>
2020-10-12 21:33:30 +0530

    Put back 2 upgrades that don't take in new parameteres

--- src/com/endicott/edu/models/AdministrativeBldgModel.java
+++ src/com/endicott/edu/models/AdministrativeBldgModel.java
@@ -1,11 +1,14 @@
 package com.endicott.edu.models;
 
+import java.util.ArrayList;
+
 public class AdministrativeBldgModel extends BuildingModel{
     //inherits from BuildingModel
     public AdministrativeBldgModel(String name){
         super(name, BuildingType.admin().getType());
 //        this.getUpgrades().add(new Upgrade("Tables", 0, 0, "Upgraded the Tables in admin offices!", UpgradeEvent.getTableEvents(), 2, true));
-//        this.getUpgrades().add(new Upgrade("Bursars", 205, 0));
+        this.getUpgrades().add(new Upgrade("Bursars", 205, 0));
+        this.getUpgrades().add(new Upgrade("CommonRooms", 5000, 50));
 //        this.getUpgrades().add(new Upgrade("Cubicles", 320, 0, "Upgraded the office cubicles in the admin", UpgradeEvent.getCubicleEvents(), 3, true));
 //        this.getUpgrades().add(new Upgrade("MeetingHalls", 320, 0, "Upgraded the meeting rooms in the admin", UpgradeEvent.getMeetingRoomEvents(), 3, true));
     }
